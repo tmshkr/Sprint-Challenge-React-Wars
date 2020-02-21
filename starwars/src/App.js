4b9347571795a43bf1ea6640841637deabfc7e76
Tim Shaker <tim@tmshkr.com>
2020-02-21 08:20:22 -0800

    fetch data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [currentPage, setPage] = useState(1);
+  const [data, setData] = useState({});
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    const url = `https://swapi.co/api/people/?page=${currentPage}&format=json`;
+    axios.get(url).then(({ data }) => {
+      console.log(data);
+      setData(data);
+    });
+  }, [currentPage]);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
     </div>
   );
-}
+};
 
 export default App;
